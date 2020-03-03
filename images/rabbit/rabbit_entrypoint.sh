0641d28d1256f2c8ea565b3198a9ec137a00b534
Antonio Defina <defina@ismb.it>
2020-03-03 18:56:12 +0100

    [Volumes] Removed unused external volumes

--- images/rabbit/rabbit_entrypoint.sh
+++
@@ -1,4 +0,0 @@
-rabbitmq-plugins enable rabbitmq_management
-rabbitmqctl add_user monica monica123
-rabbitmqctl set_user_tags monica administrator
-rabbitmqctl set_permissions -p / monica /".*/" /".*/" /".*/"
--- images/redis/redis_run.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-set -e
-
-echo never | tee /sys/kernel/mm/transparent_hugepage/enabled
-echo never | tee /sys/kernel/mm/transparent_hugepage/defrag
